1c888942666e4c106c47799d45aead832b66a737
Oviya1127 <oviyaovi403@gmail.com>
2026-05-09 19:36:08 +0530

    fix: update postgres host for k8s

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://a771fb8d8395d4e48bb768a3ddbde378-15164050.ap-south-1.elb.amazonaws.com";
+const API_BASE_URL = "http://a771fb8d8395d4e48bb768a3ddbde378-15164050.ap-south-1.elb.amazonaws.com/api";
 const TokenManager = {
     getToken() { return localStorage.getItem('access_token'); },
     setToken(token) { localStorage.setItem('access_token', token); },
